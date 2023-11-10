94086464814d05c22f852435b054fbdb73d849be
santicr <santicr@javerianacali.edu.co>
2023-11-10 08:07:25 -0500

    Documentación añadida

--- story_generator/core/views.py
+++ story_generator/core/views.py
@@ -4,10 +4,19 @@ from transformers import TextGenerationPipeline, GPT2LMHeadModel, AutoTokenizer
 from translate import Translator
 from dotenv import dotenv_values
 
-# Create your views here.
+# Renderización del template index
 def index(request):
     return render(request, "core/index.html")
 
+"""
+Entradas: Género de historias.
+
+Esta función lee la base de datos aspis/gpt2-genre-story-generation, en donde hay historias
+generadas junto con sus géneros y características. Posteriormente, con un modelo pre-entrenado,
+como gpt2, lee la base de datos para generar una historia totalmente nueva.
+
+Salidas: Historia nueva generada a partir de del género ingresado
+"""
 def generar_historia(genre):
     model_name = "aspis/gpt2-genre-story-generation"
 
@@ -20,6 +29,14 @@ def generar_historia(genre):
     
     return story
 
+"""
+Entradas: Historia generada
+
+Esta función recibe la historia generada anteriormente, sin embargo, esta historia tiene palabras
+innecesarias que se decide remover para solo obtener la historia.
+
+Salidas: Obtiene la misma historia pero removiendo las palabras del género y BOS
+"""
 def arreglar_historia(historia):
     nueva_historia = ""
     historia = historia.split()
@@ -31,6 +48,15 @@ def arreglar_historia(historia):
 
     return nueva_historia
 
+"""
+Entradas: Cualquier texto en inglés
+
+Esta función recibe cualquier texto en inglés y se traduce con Translator, sin embargo,
+esto tiene una limitación, solo puede recibir 500 caracteres, por lo que, si tiene más de estos,
+la función segmenta hasta encontrar ese límite para traducir por segmentos.
+
+Salidas: El mismo texto traducido al español
+"""
 def traducir_texto(texto_ingles):
     translator = Translator(from_lang='en', to_lang='es')
     max_chars = 500  # Máximo de caracteres permitidos por la biblioteca
@@ -49,7 +75,14 @@ def traducir_texto(texto_ingles):
 
     return texto_espanol
 
+"""
+Entradas: El request por parte del front
 
+Esta función es la que recibe las entradas del usuario desde el frontend y hace el llamado
+de las funciones anteriores para obtener la historia y mostrarla en pantalla!
+
+Salidas: Historia generada a partir del llamado de las funciones anteriores
+"""
 def generate_story(request):
     if request.method == 'POST':
         genres = ["romance", "adventure", "mystery-&-detective", "fantasy", "humor-&-comedy", "paranormal", "science-fiction"]
